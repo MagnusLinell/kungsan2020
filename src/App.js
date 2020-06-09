e8f42130e18d06210f091ac8d71d78174f7d5a73
Magnus Linell <magnus.linell@gmail.com>
2020-06-09 23:10:44 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>#kungsan2020 - Kungsleden 2020</h1>
+        <p>Även om resan är målet så är de sjukt skönt att komma i mål!</p>
+        <p>Min resa startar ca 10 juli 2020. Mitt mål:</p>
+        <p>Att gå kunglsedan med all packning från start till mål.</p>
       </header>
     </div>
   );
