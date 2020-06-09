f70a31f43f8bd7a3798291a6715c6a0d2dbcbeaa
Magnus Linell <magnus.linell@gmail.com>
2020-06-09 23:21:15 +0200

    add stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>Målet</li>
+          <li>Packningen</li>
+          <li>Maten</li>
+          <li>Rutten</li>
+          <li>Turen</li>
+        </ul>
+      </nav>
       <header className="App-header">
         <h1>#kungsan2020 - Kungsleden 2020</h1>
+      </header>
+      <main>
         <p>Även om resan är målet så är de sjukt skönt att komma i mål!</p>
         <p>Min resa startar ca 10 juli 2020. Mitt mål:</p>
         <p>Att gå kunglsedan med all packning från start till mål.</p>
-      </header>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
